0cd3262dc3780403e99afcb2e4606d1965647bd3
zachharris1 <69470481+zachharris1@users.noreply.github.com>
2020-11-12 16:15:37 -0500

    Merge Alpha into Master (#2)
    * fix package data dir
    
    * fix for bad setup.py configs
    
    * bump version
    
    * add circleci config
    
    * circleci typo
    
    * Trigger circleci
    
    * fix circleci config
    
    * fix circleci
    
    * add Makefile
    
    * add pylint and ipdb to setup.py
    
    * removed failing pylint
    
    * deleted makefile
    
    Co-authored-by: Drew Banin <drew@fishtownanalytics.com>
    Co-authored-by: Kyle Allan <kyle@stitchdata.com>

--- setup.py
+++ setup.py
@@ -3,22 +3,28 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-ebay',
-      version='0.0.1',
+      version='0.0.2',
       description='Singer.io tap for extracting data from the Ebay API',
       author='Fishtown Analytics',
       url='http://fishtownanalytics.com',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tab_ebay'],
+      py_modules=['tap_ebay'],
       install_requires=[
           'tap-framework==0.1.1',
       ],
+      extras_require={
+        'dev': [
+            'ipdb==0.11',
+            'pylint==2.4.4',
+        ]
+      },
       entry_points='''
           [console_scripts]
           tap-ebay=tap_ebay:main
       ''',
       packages=find_packages(),
       package_data={
-          'tap_lever': [
+          'tap_ebay': [
               'schemas/*.json'
           ]
       })
